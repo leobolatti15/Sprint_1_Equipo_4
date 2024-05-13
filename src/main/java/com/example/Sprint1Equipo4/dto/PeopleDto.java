b183f4c1c44c6b22b536a6da1cf80daf10b519dc
Matias <matias.oggero@sancorsalud.com.ar>
2024-05-13 16:58:22 -0300

    add peopleDto

--- src/main/java/com/example/Sprint1Equipo4/dto/PeopleDto.java
+++ src/main/java/com/example/Sprint1Equipo4/dto/PeopleDto.java
@@ -1,4 +1,18 @@
 package com.example.Sprint1Equipo4.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PeopleDto {
+
+    private String dni;
+    private String name;
+    private String lastName;
+    private String birthDate;
+    private String email;
+
 }
