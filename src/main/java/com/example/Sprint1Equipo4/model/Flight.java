1cff03607c703e6398cdd282cbd49da0f6e90f66
josejosefina <josefina.eberhardt@gmail.com>
2024-05-14 09:58:03 -0300

    Agrego modelo de vuelo

--- src/main/java/com/example/Sprint1Equipo4/model/Flight.java
+++ src/main/java/com/example/Sprint1Equipo4/model/Flight.java
@@ -1,4 +1,25 @@
 package com.example.Sprint1Equipo4.model;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Flight {
+    private String flightNumber;
+    private String origin;
+    private String destination;
+    private String seatType;
+    private Integer pricePerPerson;
+    @JsonFormat(pattern ="dd/MM/yyyy")
+    private LocalDate departureDate;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate returnDate;
 }
