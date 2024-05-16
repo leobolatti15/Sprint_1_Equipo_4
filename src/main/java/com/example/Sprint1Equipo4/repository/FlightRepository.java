78328f33c397b809409643aa21d200d28befb68e
Maca Ruffa <101282037+MacarenaRuffa@users.noreply.github.com>
2024-05-16 15:08:19 -0300

    fix fligth

--- src/main/java/com/example/Sprint1Equipo4/repository/FlightRepository.java
+++ src/main/java/com/example/Sprint1Equipo4/repository/FlightRepository.java
@@ -1,13 +1,7 @@
 package com.example.Sprint1Equipo4.repository;
-
 import com.example.Sprint1Equipo4.model.Flight;
-
 import java.util.List;
 
 public interface FlightRepository {
     List<Flight> findAll();
-
-    interface FlightRepository {
-        List<Flight> findAll();
-    }
 }
