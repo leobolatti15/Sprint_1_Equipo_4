1ddb5630e5e441fa131e84a65cd3757dcb75eab1
matiasoggero <matiassoggero@gmail.com>
2024-06-07 07:43:29 -0300

    agrego test reserve

--- src/main/java/com/example/Sprint1Equipo4/service/FlightService.java
+++ src/main/java/com/example/Sprint1Equipo4/service/FlightService.java
@@ -1,6 +1,7 @@
 package com.example.Sprint1Equipo4.service;
 
 import com.example.Sprint1Equipo4.dto.request.FlightReqDto;
+import com.example.Sprint1Equipo4.dto.request.FlightReservationDto;
 import com.example.Sprint1Equipo4.dto.response.FlightDTO;
 import com.example.Sprint1Equipo4.dto.response.FlightResDto;
 import com.example.Sprint1Equipo4.model.Flight;
@@ -25,5 +26,7 @@ public interface FlightService {
    Flight upDate(FlightDTO flight);
 
    void validateDateRangeFlight(LocalDate dateFrom, LocalDate dateTo, String destination);
+
+   Flight getFlight(List<Flight> allFlight, FlightReservationDto reservation);
 }
 
